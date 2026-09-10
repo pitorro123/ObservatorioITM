13745ec3803fc48857398a420c76b24885193f73
pitorro123 <pipe.kmx@hotmail.com>
2026-09-10 12:39:04 -0500

    fix: navbar INICIO y enlaces reales de redes sociales del ITM

--- src/data/navegacion-publica.js
+++ src/data/navegacion-publica.js
@@ -1,5 +1,5 @@
 export const enlacesNavegacionPublica = [
-  { etiqueta: "NOTICIAS", ruta: "/" },
+  { etiqueta: "INICIO", ruta: "/" },
   { etiqueta: "EVENTOS", ruta: "/eventos" },
   { etiqueta: "SEMILLERO", ruta: "/semillero" },
   { etiqueta: "GALERÍA", ruta: "/galeria" },
@@ -7,7 +7,7 @@ export const enlacesNavegacionPublica = [
 ];
 
 export const redesSociales = [
-  { nombre: "YouTube", icono: "Youtube", enlace: "#" },
-  { nombre: "Facebook", icono: "Facebook", enlace: "#" },
-  { nombre: "Instagram", icono: "Instagram", enlace: "#" },
+  { nombre: "YouTube", icono: "Youtube", enlace: "https://www.youtube.com/@itminstitucional" },
+  { nombre: "Facebook", icono: "Facebook", enlace: "https://www.facebook.com/ITMinstitucional" },
+  { nombre: "Instagram", icono: "Instagram", enlace: "https://www.instagram.com/itminstitucional" },
 ];
